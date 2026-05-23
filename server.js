59e99f136af8b907e8b0bca408e1c036f24bc50a
Absar Ali <absar@gmail.com>
2026-05-23 10:58:35 +0500

    fixed homepage

--- server.js
+++ server.js
@@ -7,45 +7,54 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("PASTE_YOUR_MONGODB_LINK_HERE")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://<softwareweb248_db_user>:<RgULyQ6Eg6T9HD3t>@create-deployment.bmcdw3l.mongodb.net/?appName=Create-Deployment")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
 
 const EntrySchema = new mongoose.Schema({
-  date: String,
-  description: String,
-  rate: Number,
-  qty: Number,
-  amount: Number,
-  company: String,
-  project: String,
-  partyName: String,
-  partyNumber: String,
-  givenPayment: Number,
-  remainingPayment: Number,
-  status: String,
-  note: String
+
+    date:String,
+    description:String,
+    rate:Number,
+    qty:Number,
+    amount:Number,
+    company:String,
+    project:String,
+    partyName:String,
+    partyNumber:String,
+    givenPayment:Number,
+    remainingPayment:Number,
+    status:String,
+    note:String
+
 });
 
 const Entry = mongoose.model("Entry", EntrySchema);
 
-app.post("/save", async (req, res) => {
-  try {
-    const entry = new Entry(req.body);
-    await entry.save();
-    res.json({ success: true });
-  } catch (err) {
-    res.json({ success: false });
-  }
+app.get("/getData", async(req,res)=>{
+
+    const data = await Entry.find();
+
+    res.json(data);
+
 });
 
-app.get("/entries", async (req, res) => {
-  const data = await Entry.find();
-  res.json(data);
+app.post("/addData", async(req,res)=>{
+
+    const newData = new Entry(req.body);
+
+    await newData.save();
+
+    res.json({message:"Data Saved"});
+
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log("Server Running");
+app.listen(PORT, ()=>{
+
+    console.log("Server Running");
+
 });
